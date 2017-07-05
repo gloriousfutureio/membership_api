e91ab7e51bdd80465dbcd0b6081a74e4374d2c3c
Jeff May <jeff.n.may@gmail.com>
2017-07-04 22:08:07 -0700

    Work on GraphQL queries.

--- membership/schemas/graphql.py
+++ membership/schemas/graphql.py
@@ -1,8 +1,16 @@
+from typing import Any, Dict, Iterable
+
 import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType
+from graphql.execution.base import ResolveInfo
+from sqlalchemy.orm.query import Query
 
 from membership.database import models
+from membership.database.base import Base
+from membership.database.models import *
 
+
+# Full database models
 
 class MemberSchema(SQLAlchemyObjectType):
     class Meta:
@@ -54,57 +62,104 @@ class EligibleVoterSchema(SQLAlchemyObjectType):
         model = models.EligibleVoter
 
 
-class Query(graphene.ObjectType):
-    attendees = graphene.List(AttendeeSchema)
-    candidates = graphene.List(CandidateSchema)
-    committees = graphene.List(CommitteeSchema)
-    elections = graphene.List(ElectionSchema)
-    eligible_voters = graphene.List(EligibleVoterSchema)
-    meetings = graphene.List(MeetingSchema)
-    members = graphene.List(MemberSchema)
-    rankings = graphene.List(RankingSchema)
-    roles = graphene.List(RoleSchema)
-    votes = graphene.List(VoteSchema)
+# API View Models
 
-    def resolve_attendees(self, args, context, info):
-        query = AttendeeSchema.get_query(context)
+class AnonMemberFields(graphene.AbstractType):
+    id = graphene.String()
+    eligible_votes = graphene.List(EligibleVoterSchema)
+    meetings_attended = graphene.List(MeetingSchema)
+
+
+class AnonMemberSchema(graphene.ObjectType, AnonMemberFields):
+    pass
+
+
+class QuerySchema(graphene.ObjectType):
+    all_attendees = graphene.List(AttendeeSchema)
+    all_candidates = graphene.List(CandidateSchema)
+    all_committees = graphene.List(CommitteeSchema)
+    all_elections = graphene.List(ElectionSchema)
+    all_eligible_voters = graphene.List(EligibleVoterSchema)
+    all_meetings = graphene.List(MeetingSchema)
+    all_members = graphene.List(MemberSchema)
+    all_rankings = graphene.List(RankingSchema)
+    all_roles = graphene.List(RoleSchema)
+    all_votes = graphene.List(VoteSchema)
+
+    anon_members = graphene.List(AnonMemberSchema)
+
+    my_user = graphene.Field(MemberSchema)
+    my_meetings = graphene.List(MeetingSchema)
+
+    def resolve_all_attendees(self, args: dict, context: dict, info: ResolveInfo):
+        query: Query = AttendeeSchema.get_query(context)
         return query.all()
 
-    def resolve_candidates(self, args, context, info):
-        query = CandidateSchema.get_query(context)
+    def resolve_all_candidates(self, args: dict, context: dict, info: ResolveInfo):
+        query: Query = CandidateSchema.get_query(context)
         return query.all()
 
-    def resolve_committees(self, args, context, info):
-        query = CommitteeSchema.get_query(context)
+    def resolve_all_committees(self, args: dict, context: dict, info: ResolveInfo):
+        query: Query = CommitteeSchema.get_query(context)
         return query.all()
 
-    def resolve_elections(self, args, context, info):
-        query = ElectionSchema.get_query(context)
+    def resolve_all_elections(self, args: dict, context: dict, info: ResolveInfo):
+        query: Query = ElectionSchema.get_query(context)
         return query.all()
 
-    def resolve_eligible_voters(self, args, context, info):
-        query = EligibleVoterSchema.get_query(context)
+    def resolve_all_eligible_voters(self, args: dict, context: dict, info: ResolveInfo):
+        query: Query = EligibleVoterSchema.get_query(context)
         return query.all()
 
-    def resolve_meetings(self, args, context, info):
-        query = MeetingSchema.get_query(context)
+    def resolve_all_meetings(self, args: dict, context: dict, info: ResolveInfo):
+        query: Query = MeetingSchema.get_query(context)
         return query.all()
 
-    def resolve_members(self, args, context, info):
-        query = MemberSchema.get_query(context)
+    def resolve_all_members(self, args: dict, context: dict, info: ResolveInfo):
+        requester: Member = context['requester']
+        if requester.is_admin():
+            query: Query = MemberSchema.get_query(context)
+            return query.all()
+        else:
+            return [requester]
+
+    def resolve_anon_members(self, args: dict, context: dict, info: ResolveInfo):
+        query: Query = MemberSchema.get_query(context)
+        filters = filters_for(Member, args)
+        all: Iterable[Member] = query.filter(*filters)
+        result: Iterable[AnonMemberSchema] = [AnonMemberSchema(
+            id=m.id,
+            eligible_votes=m.eligible_votes,
+            meetings_attended=m.meetings_attended
+        ) for m in all]
+        return result
+
+    def resolve_all_rankings(self, args: dict, context: dict, info: ResolveInfo):
+        query: Query = RankingSchema.get_query(context)
         return query.all()
 
-    def resolve_rankings(self, args, context, info):
-        query = RankingSchema.get_query(context)
+    def resolve_all_roles(self, args: dict, context: dict, info: ResolveInfo):
+        query: Query = RoleSchema.get_query(context)
         return query.all()
 
-    def resolve_roles(self, args, context, info):
-        query = RoleSchema.get_query(context)
+    def resolve_all_votes(self, args: dict, context: dict, info: ResolveInfo):
+        query: Query = VoteSchema.get_query(context)
         return query.all()
 
-    def resolve_votes(self, args, context, info):
-        query = VoteSchema.get_query(context)
-        return query.all()
+    def resolve_my_user(self, args: dict, context: dict, info: ResolveInfo):
+        requester: Member = context['requester']
+        return requester
+
+    def resolve_my_meetings(self, args: dict, context: dict, info: ResolveInfo):
+        print('args = {}'.format(args))
+        print('resolve info = {}'.format(info))
+        requester: Member = context['requester']
+        query: Query = MeetingSchema.get_query(context)
+        return query.filter(requester.id in Meeting.attendees)
 
 
-schema = graphene.Schema(query=Query)
+def filters_for(model: Base, args: Dict[str, Any]):
+    return [getattr(model, k) == v for k, v in args.items() if v is not None]
+
+
+schema = graphene.Schema(query=QuerySchema)
--- membership/web/graphql.py
+++ membership/web/graphql.py
@@ -1,18 +1,18 @@
-from flask import Blueprint, jsonify, request, Response
+import json
+
+from flask import Blueprint, jsonify, request
 
 from membership.database.base import Session
 from membership.database.models import Member
 from membership.schemas.graphql import schema
-from membership.web.auth import requires_auth
-from typing import List
-import traceback
-import json
+from membership.web.auth import authentication, error_response
+from typing import Optional
 
 graphql_api = Blueprint('graphql_api', __name__)
 
 
 @graphql_api.route('/graphql', methods=['GET', 'POST'])
-@requires_auth(admin=False)
+@authentication(required=True)
 def query(requester: Member, session: Session):
     # this should match the following spec:
     # http://graphql.org/learn/serving-over-http/#http-methods-headers-and-body
@@ -26,14 +26,14 @@ def query(requester: Member, session: Session):
         graphql_query = request.args.get('query', '')
     # if not a GET request, handle the ambiguity of multiple query parameters
     elif 'query' in request.args and 'query' in request.json:
-        return Response(json.dumps({'errors': ['"query" parameter cannot be in both query string and request body']}), 400)
+        return error_response(400, ['"query" parameter cannot be in both query string and request body'])
     # from the request body as json if POST request
     else:
         graphql_query = request.json.get('query', '')
 
     # validate the query
     if graphql_query == '':
-        return Response(json.dumps({'errors': ['GraphQL query cannot be empty']}), 400)
+        return error_response(400, ['GraphQL query cannot be empty'])
 
     # get the variables
     # from the query string if POST request
@@ -44,17 +44,11 @@ def query(requester: Member, session: Session):
         graphql_variables = request.json.get('variables', {})
 
     # execute the query with the provided variables
-    result = schema.execute(graphql_query, context_value={'session': session}, variable_values=graphql_variables)
-    errors = result.errors  # type: List[Exception]
-    if errors:
-        error_msgs = [
-            {
-                'type': type(e).__name__,
-                'message': str(e),
-                # TODO: Only show traceback if development mode
-                'traceback': traceback.format_exception(None, e, e.__traceback__),
-            } for e in errors
-        ]
-        return jsonify({'errors': error_msgs})
+    result = schema.execute(graphql_query, context_value={
+        'session': session,
+        'requester': requester,
+    }, variable_values=graphql_variables)
+    if result.errors:
+        return error_response(400, result.errors)
     else:
         return jsonify({'data': result.data})
